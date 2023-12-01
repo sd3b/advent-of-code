c94ee41d2035bbb7a01b36ca84144c7ff2a71429
AES-128 <119679392+AES-128@users.noreply.github.com>
2023-12-01 11:12:55 +0000

    2023 Day 01

---
+++ 2023/day_01/part_1.py
@@ -0,0 +1,12 @@
+values_total = 0
+
+for line in open("../inputs/day_01"):
+    value = ""
+
+    for char in line:
+        if char.isdigit():
+            value += char
+
+    values_total += int(value[0] + value[-1])
+
+print("Part 1:", values_total)
---
+++ 2023/day_01/part_2.py
@@ -0,0 +1,28 @@
+digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+total = 0
+
+for line in open("../inputs/day_01"):
+    idx = 0
+    value = ""
+
+    while idx < len(line):
+        if line[idx].isdigit():
+            value += line[idx]
+            idx += 1
+        else:
+            old_idx = idx + 1
+            current_str = ""
+
+            while idx < len(line):
+                current_str += line[idx]
+                idx += 1
+
+                if current_str in digits:
+                    value += str(digits.index(current_str) + 1)
+                    break
+
+            idx = old_idx
+
+    total += int(value[0] + value[-1])
+
+print("Part 2:", total)
