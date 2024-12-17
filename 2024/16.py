2887cf798efd99aa37a0b3fb4f6bb1e46727bd47
sd3b <119679392+sd3b@users.noreply.github.com>
2024-12-17 00:18:27 +0000

    bytes instead of sets for less mem. allocating [551.5 ms ± 72.0 ms] (should backtrack probably)

--- 2024/16.py
+++ 2024/16.py
@@ -7,14 +7,16 @@ for y, line in enumerate(f):
     grid.append(line)
 
     for x, char in enumerate(line):
-        if char == "S":     pq = [(0, y, x, 0, {(y, x)})]
+        if char == "S":     pq = [(0, y, x, 0, bytes([y, x]))]
         elif char == "E":   dest = (y, x)
 
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 distances = {}
 
 best_score = float("inf")
-best_paths = set()
+best_paths = bytes()
+
+# y and x should fit in a byte each
 
 while pq:
     score, y, x, idx, path = heappop(pq)
@@ -24,15 +26,16 @@ while pq:
 
     if (y, x) == dest:
         best_score = min(best_score, score)
-        best_paths |= path
+        best_paths += path
 
     for jdx in [(idx - 1) % 4, idx, (idx + 1) % 4]:
         dy, dx = dirs[jdx]
         ny, nx = y + dy, x + dx
-        new_score = score + 1 + 1000 * int(idx != jdx)
+        new_score = score + 1 + 1000 * (1 if idx != jdx else 0)
 
         if grid[ny][nx] != "#" and new_score < distances.get((ny, nx, jdx), float("inf")):
-            heappush(pq, (new_score, ny, nx, jdx, path | {(ny, nx)}))
+            heappush(pq, (new_score, ny, nx, jdx, path + bytes([ny, nx])))
 
 print("Part A:", best_score)
-print("Part B:", len(best_paths))
+coords = set(best_paths[idx:idx+2] for idx in range(0, len(best_paths), 2))
+print("Part B:", len(coords))
